924659a2d28b63d98334f33e16ffbc590bbe877b
Kitty <kittykumas@gmail.com>
2018-07-06 01:42:05 -0700

    installed the chartjs npm; initial the chart on theChart page with hard coded data

--- client/src/pages/Chart/Chart.js
+++ client/src/pages/Chart/Chart.js
@@ -2,6 +2,7 @@ import React ,{ Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
+import AChart from 'chart.js';
 import "./Chart.css"
 
 class Chart extends Component {
@@ -15,6 +16,7 @@ class Chart extends Component {
 
   componentDidMount() {
       this.loadJobs();
+      this.testing();
   }
 
   loadJobs = () => {
@@ -27,15 +29,46 @@ class Chart extends Component {
       .catch(err => console.log(err));
   };
 
-  deleteJob = (id) =>{
-    API.delUserJob(id)
-    .then(res=>{
-      console.log(res.data);
-      this.loadJobs();
-    })
-    .catch(err =>console.log(err))
+  testing =() =>{
+    var ctx = document.getElementById("userChart");
+    // ctx.innerHTML= "kjflsfjlsd";
+    var myChart = new AChart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
   }
-
 
   render() {
 
@@ -43,9 +76,11 @@ class Chart extends Component {
         <Container fluid>
           <Row>
           <Col size="md-12">
-            <Jumbotron id="dashJumbo">
-              <h3>User charts comparison </h3>
-                
+            <Jumbotron >
+              <div id="chartJumbo">
+                <h3>User charts comparison </h3>
+                <canvas id="userChart"></canvas>
+              </div>
             </Jumbotron>
           </Col>
           </Row>
